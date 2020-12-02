8cf503f2caa02ca35d0f156063ef01670cb21ae2
Alex Pham <alex.pham468@gmail.com>
2020-12-02 21:20:30 +1100

    initial

---
+++ public/precache-manifest.c97e6fc95d41beead88f744757bd23b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6944c7993eb13eb219c4009dcae15a85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09cd24e7be0c2f3b8306",
+    "url": "/static/css/main.9c405617.chunk.css"
+  },
+  {
+    "revision": "fba86d7f2d2209a65470",
+    "url": "/static/js/2.f35cbbc6.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/static/js/2.f35cbbc6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09cd24e7be0c2f3b8306",
+    "url": "/static/js/main.d511cc37.chunk.js"
+  },
+  {
+    "revision": "7b88067db00cda3cdf04",
+    "url": "/static/js/runtime-main.b20d7a2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
